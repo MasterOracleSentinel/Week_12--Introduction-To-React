2a21493ca0c134da56c539e3dc5a30c1928928d2
K. Artis-Mickens <105542222+MasterOracleSentinel@users.noreply.github.com>
2023-01-21 23:39:22 -0500

    Create todo.jsx

---
+++ TODO List Delete Assignment/todo.jsx
@@ -0,0 +1,89 @@
+class TodoApp extends React.Component {
+  state = {
+    tasks: ["task 1", "task 2", "task 3"],
+  };
+
+  handleSubmit = (task) => {
+    this.setState({ tasks: [...this.state.tasks, task] });
+  };
+
+  // TODO: Add handleDelete to remove the index from the state
+
+//   Inside the TodoApp component, define the handleDelete function. This function takes in an index representing the item to delete.
+  handleDelete = (indexofDeletedItem) =>{
+    // Use splice to remove the item from the state.
+    let newState = state.splice(indexofDeletedItem) => item != index);
+    // Set the new state using the function setState.
+    setState(newState;)
+  }
+
+
+
+
+
+
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="card frame">
+          <SubmitForm onFormSubmit={this.handleSubmit} />
+          <TodoList tasks={this.state.tasks} onDelete={this.handleDelete} />
+        </div>
+      </div>
+    );
+  }
+}
+
+class SubmitForm extends React.Component {
+  state = { item: "" };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.item === "") return;
+    this.props.onFormSubmit(this.state.item);
+    this.setState({ item: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          className="input"
+          placeholder="Enter Item"
+          value={this.state.item}
+          onChange={(e) => this.setState({ item: e.target.value })}
+        />
+        <button className="button">Add</button>
+      </form>
+    );
+  }
+}
+
+const TodoList = (props) => {
+  const todos = props.tasks.map((todo, index) => {
+    return (
+      <Todo content={todo} key={index} id={index} onDelete={props.onDelete} />
+    );
+  });
+  return <div className="list-wrapper">{todos}</div>;
+};
+
+const Todo = (props) => {
+  return (
+    <div id={props.id * 10} className="list-item test-item">
+      {props.content}
+      <button
+        id={props.id}
+        className="delete is-pulled-right"
+        onClick={() => {
+          props.onDelete(props.id);
+        }}
+      >
+        X
+      </button>
+    </div>
+  );
+};
+
+ReactDOM.render(React.createElement(TodoApp), document.getElementById("root"));
